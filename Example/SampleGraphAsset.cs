cd131357cb241c9a205c1492b03fd51a8628a695
HalfLobsterMan <766439226@qq.com>
2021-10-21 21:26:03 +0800

    增加简单案例

---
+++ Example/SampleGraphAsset.cs
@@ -0,0 +1,25 @@
+#region 注 释
+/***
+ *
+ *  Title:
+ *  
+ *  Description:
+ *  
+ *  Date:
+ *  Version:
+ *  Writer: 半只龙虾人
+ *  Github: https://github.com/HalfLobsterMan
+ *  Blog: https://www.crosshair.top/
+ *
+ */
+#endregion
+using CZToolKit.GraphProcessor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SampleGraphAsset : BaseGraphAsset<SampleGraph>
+{
+
+}
---
+++ Example/SampleNode.cs
@@ -0,0 +1,24 @@
+#region 注 释
+/***
+ *
+ *  Title:
+ *  
+ *  Description:
+ *  
+ *  Date:
+ *  Version:
+ *  Writer: 半只龙虾人
+ *  Github: https://github.com/HalfLobsterMan
+ *  Blog: https://www.crosshair.top/
+ *
+ */
+#endregion
+using CZToolKit.GraphProcessor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleNode : BaseNode
+{
+
+}
