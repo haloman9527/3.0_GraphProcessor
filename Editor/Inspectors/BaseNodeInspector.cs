d2ee6da1f80c520566f1e943d8725958878a651b
HalfLobsterMan <766439226@qq.com>
2022-05-18 16:41:42 +0800

    update

--- Editor/Inspectors/BaseNodeInspector.cs
+++ Editor/Inspectors/BaseNodeInspector.cs
@@ -45,10 +45,7 @@ namespace CZToolKit.GraphProcessor.Editors
                 EditorGUI.BeginChangeCheck();
                 property.Draw();
                 if (EditorGUI.EndChangeCheck() && view.Model.TryGetValue(property.Name, out var bindableProperty))
-                {
                     bindableProperty.SetValueWithNotify(property.ValueEntry.WeakSmartValue);
-                    Editor.Repaint();
-                }
             }
             propertyTree.EndDraw();
             Editor.Repaint();
